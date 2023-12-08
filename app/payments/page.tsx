16d87b924066e05180b3e98a166bce05f66792f6
solly-awaed <123766905+solly-awaed@users.noreply.github.com>
2023-12-08 20:52:47 +0200

    before add cards

--- app/payments/page.tsx
+++ app/payments/page.tsx
@@ -1,17 +1,11 @@
 import * as React from "react";
-import CreditCardInput, { CreditCardData } from "../ui/credit-card";
 
 interface IInvoicesProps {}
 
 const Invoices: React.FunctionComponent<IInvoicesProps> = (props) => {
-	const handleCardDataChange = (cardData: CreditCardData) => {
-		console.log(cardData); // You can access the card data here
-	};
 	return (
 		<div className="bg-gray-200 min-h-screen flex p-[5rem]">
-			<div className="flex space-x-4 basis-6/12">
-				<CreditCardInput onChange={handleCardDataChange} />
-			</div>
+			<div className="flex space-x-4 basis-6/12"></div>
 		</div>
 	);
 };
