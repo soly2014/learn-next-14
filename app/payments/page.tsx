d91c4c37b759739bff67cdaa17c472f34d87f8bf
solly-awaed <123766905+solly-awaed@users.noreply.github.com>
2023-12-08 20:32:31 +0200

    d

--- app/payments/page.tsx
+++ app/payments/page.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
+import CreditCardInput, { CreditCardData } from "../ui/credit-card";
 
 interface IInvoicesProps {}
 
 const Invoices: React.FunctionComponent<IInvoicesProps> = (props) => {
-	return <p>Invoices page</p>;
+	const handleCardDataChange = (cardData: CreditCardData) => {
+		console.log(cardData); // You can access the card data here
+	};
+	return (
+		<div className="bg-gray-200 min-h-screen flex p-[5rem]">
+			<div className="flex space-x-4 basis-6/12">
+				<CreditCardInput onChange={handleCardDataChange} />
+			</div>
+		</div>
+	);
 };
 
 export default Invoices;
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,27 @@
 import * as React from "react";
+import Image from "next/image";
 
 interface ICustomersProps {}
 
 const Customers: React.FunctionComponent<ICustomersProps> = (props) => {
-	return <p>Customers page</p>;
+	return (
+		<div className="bg-white shadow-md rounded px-8 py-6 mt-4">
+			<div className="text-center">
+				<Image
+					src={"https://i.pravatar.cc/300"}
+					alt="User Avatar"
+					className="w-32 h-32 mx-auto rounded-full"
+					width={"300"}
+					height={"300"}
+				/>
+			</div>
+			<div className="mt-4 text-center">
+				<p className="text-xl font-semibold">Soliman Mahmoud Soliman</p>
+				<p className="text-gray-500">Male</p>
+				<p className="text-gray-500">DOB: 29-7-1991</p>
+			</div>
+		</div>
+	);
 };
 
 export default Customers;
